6a3e1441ec96ae6297fff113bffaf7ecbc7e745e
root <root@hungry-mahavira.190-14-232-141.plesk.page>
2026-03-05 20:38:18 -0500

    fix: enable CORS for staging frontend

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['http://190.14.233.186:3001', 'http://190.14.233.186:3000'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  });
+
   const port = Number(process.env.PORT) || 3000;
   await app.listen(port, '0.0.0.0');
 
