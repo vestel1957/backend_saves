2894af0a8128241a40129cc5f46ee23ab99dd55b
root <root@hungry-mahavira.190-14-232-141.plesk.page>
2026-03-10 07:18:42 -0500

    fix: import AuthModule in UsersModule for JwtGuard

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,10 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { UploadsModule } from '../uploads/uploads.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, UploadsModule],
+  imports: [PrismaModule, UploadsModule, AuthModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
